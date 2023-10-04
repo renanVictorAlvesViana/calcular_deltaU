d5a15e02bfd8d645ab62b63904171f75d2e90a77
Renanzinho <acontadorenam@gmail.com>
2023-10-03 23:24:53 -0300

    a

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# ∆U = Q + T
+# ∆U = Q - T
+from tkinter import *
 
-# Calor cedido : ∆U = - Q - T
-# Calor recebido: ∆U = Q - T
-# Trabalho sobre o gás: ∆U= Q + T
-# Trabalho pelo gás: ∆U= Q - T
-
-ifCalor = int(input("O calor é cedido(0) ou recebido(1) pelo gás? [0/1] "))
-ifTrabalho = int(input("O trabalho é sobre o gaś(0) ou realizado(1) pelo gás? [0/1] "))
- 
+deltaU = 0
 
 Q = float(input("Informe o valor do calor: "))
 T = float(input("Informe valor do trabalho: "))
 
+deltaU = Q - T
 
-if ifCalor == 0 and ifTrabalho == 0:
-    print("certo")
+print("A variação da enegia interna do gás é ", deltaU)
+
+janela = TK()
+
+
+janela.mainloop()
